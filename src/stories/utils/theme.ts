31670ad3bc3089ab005e9ecb0f9498263453bd58
Sumiya Ushiro <48553998+sumi0820@users.noreply.github.com>
2023-01-18 16:52:22 +0100

    Added color palette

---
+++ src/stories/utils/theme.ts
@@ -0,0 +1,50 @@
+export const theme = {
+  buttons: {
+    size: {
+      md: { height: '36px', minWidth: '130px', padding: '0 24px' },
+      lg: { height: '52px', minWidth: '240px', padding: '0 48px' },
+    },
+  },
+  colors: {
+    main: '#38C6F4',
+    dark: '#1F5B92',
+    light: '#FF9169',
+    warning: '#DC2626',
+
+    textBlack: '#212121',
+    textGray: '#606060',
+    textGrayLight: '#b2bbc0',
+    textLink: '#38C6F4',
+    textWhite: '#FFFFFF',
+
+    bgWhite: '#FFFFFF',
+    bgGray: '#E5E5E5',
+    bgGrayMid: '#F2F2F2',
+    bgGrayLight: '#F9F9F9',
+    bgDark: '#202020',
+    bgDarkMid: '#0F0F0F',
+    bgDarkLight: '#181818',
+
+    borderGray: '#E5E5E5',
+  },
+  fonts: {
+    fontFamily: `'Averta', 'Roboto', 'Helvetica Neue', 'Arial', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', '-apple-system', 'BlinkMacSystemFont', sans-serif`,
+    fontFamilyCode: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
+  },
+  icons: {
+    size: {
+      sm: '16',
+      md: '24',
+    },
+  },
+  identicon: {
+    size: {
+      xs: '10px',
+      sm: '16px',
+      md: '24px',
+      lg: '32px',
+      xl: '40px',
+      xxl: '48px',
+    },
+  },
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,34 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      main: '#38C6F4',
+      dark: '#1F5B92',
+      light: '#FF9169',
+      warning: '#DC2626',
+
+      textBlack: '#212121',
+      textGray: '#606060',
+      textGrayLight: '#b2bbc0',
+      textLink: '#38C6F4',
+      textWhite: '#FFFFFF',
+
+      bgWhite: '#FFFFFF',
+      bgGray: '#E5E5E5',
+      bgGrayMid: '#F2F2F2',
+      bgGrayLight: '#F9F9F9',
+      bgDark: '#202020',
+      bgDarkMid: '#0F0F0F',
+      bgDarkLight: '#181818',
+
+      borderGray: '#E5E5E5',
+    },
     extend: {},
   },
+  safelist: [
+    {
+      pattern: /^(.*?)/,
+    },
+  ],
   plugins: [],
 }
