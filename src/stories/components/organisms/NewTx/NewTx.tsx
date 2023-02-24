546c2a0b28a0a2525e58457896904ce65b71b555
Sumiya Ushiro <48553998+sumi0820@users.noreply.github.com>
2023-02-25 08:26:08 +0900

    Made modal switchable

--- src/stories/components/organisms/NewTx/NewTx.tsx
+++ src/stories/components/organisms/NewTx/NewTx.tsx
@@ -3,24 +3,39 @@ import React, { useState } from 'react'
 import { SidebarIconText } from '../../molecules/IconText/SidebarIconText'
 
 import NewTxModal from './NewTxModal'
+import ReceiveTxModal from './ReceiveTxModal'
 
 const NewTx = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const onClose = () => {
+  const [isOpenReceiveTxModal, setIsOpenReceiveTxModal] = useState(false)
+
+  const onCloseNewTxModal = () => {
     setIsOpen(!isOpen)
+    console.log('change', isOpen)
+  }
+  const handleReceiveTxModal = () => {
+    setIsOpenReceiveTxModal(!isOpenReceiveTxModal)
+    console.log('setIsOpenReceiveTxModal', isOpenReceiveTxModal)
   }
   return (
-    <div>
-      <>
-        <button
-          className='bg-bgDarkLight hover:bg-dark'
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <SidebarIconText type={'NewTx'} text={'NewTx'} />
-        </button>
-        <NewTxModal isOpen={isOpen} onClose={onClose} />
-      </>
-    </div>
+    <>
+      <button
+        className='bg-bgDarkLight hover:bg-dark'
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <SidebarIconText type={'NewTx'} text={'NewTx'} />
+      </button>
+      <NewTxModal
+        isOpen={isOpen}
+        onCloseNewTxModal={onCloseNewTxModal}
+        isOpenReceiveTxModal={isOpenReceiveTxModal}
+        handleReceiveTxModal={handleReceiveTxModal}
+      />
+      <ReceiveTxModal
+        isOpen={isOpenReceiveTxModal}
+        onClose={handleReceiveTxModal}
+      />
+    </>
   )
 }
 
--- src/stories/components/organisms/NewTx/NewTxModal.tsx
+++ src/stories/components/organisms/NewTx/NewTxModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog } from '@headlessui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import EthAddress from '../../../utils/Ethereum/EthAddress'
 import Button from '../../atoms/Button/Button'
@@ -7,14 +7,23 @@ import { Icon } from '../../atoms/Icon/Icon'
 import { Token, TokenTypes } from '../../molecules/IconText/Token'
 import { tokens } from '../Portfolio/Portfolio'
 
+import ReceiveTxModal from './ReceiveTxModal'
+
 import Spacer from '~/utils/Spacer'
 
-type Props = {
-  isOpen: boolean
-  onClose: () => void
+export type ModalProps = {
+  isOpen?: boolean
+  onCloseNewTxModal: () => void
+  isOpenReceiveTxModal: boolean
+  handleReceiveTxModal: () => void
 }
 
-const NewTxDetails = () => {
+const NewTxDetails: React.FC<ModalProps> = ({
+  isOpen,
+  onCloseNewTxModal,
+  isOpenReceiveTxModal,
+  handleReceiveTxModal,
+}) => {
   return (
     <div className='flex flex-col text-textWhite'>
       <div className='py-4 px-8 rounded-2xl bg-bgDarkLight'>
@@ -34,11 +43,20 @@ const NewTxDetails = () => {
         <Spacer size={16} axis={'vertical'} />
 
         <div className='flex flex-row justify-around'>
-          <Button btnVariant={'primary'} btnSize={'lg'} btnType={'button'}>
+          <Button
+            btnVariant={'primary'}
+            btnSize={'lg'}
+            btnType={'button'}
+            handleClick={() => {
+              onCloseNewTxModal()
+              handleReceiveTxModal()
+            }}
+          >
             <Icon type={'Send'} color={'white'} size={'xl'} />
             <Spacer size={8} axis={'horizontal'} />
             <span className='text-lg'>Send</span>
           </Button>
+
           <Button btnVariant={'border'} btnSize={'lg'} btnType={'button'}>
             <Icon type={'Receive'} color={'white'} size={'xl'} />
             <Spacer size={8} axis={'horizontal'} />
@@ -46,6 +64,7 @@ const NewTxDetails = () => {
           </Button>
         </div>
       </div>
+      <Spacer size={16} axis={'vertical'} />
 
       <span className='text-textWhite text-xl font-bold'>Portfolio</span>
       <div className='flex flex-col items-center py-4 px-8 rounded-2xl bg-bgDarkLight'>
@@ -74,30 +93,49 @@ const NewTxDetails = () => {
   )
 }
 
-const NewTxModal: React.FC<Props> = ({ isOpen, onClose }) => {
+const NewTxModal: React.FC<ModalProps> = ({
+  isOpen,
+  onCloseNewTxModal,
+  isOpenReceiveTxModal,
+  handleReceiveTxModal,
+}) => {
   return (
     <>
-      <Dialog
-        open={isOpen}
-        onClose={onClose}
-        className='fixed z-10 inset-0 overflow-y-auto'
-      >
-        <div className='flex items-center justify-center min-h-screen'>
-          <Dialog.Overlay
-            className='fixed inset-0 bg-neutral-900 opacity-30'
-            aria-hidden='true'
-          />
-          <Dialog.Panel className='relative bg-bgDarkMid rounded-2xl py-6 px-8'>
-            <span className='text-textWhite text-2xl font-bold'>Header</span>
+      {isOpen && (
+        <Dialog
+          open={isOpen}
+          onClose={() => null}
+          className='fixed z-10 inset-0 overflow-y-auto'
+          static
+        >
+          <div className='flex items-center justify-center min-h-screen'>
+            <Dialog.Overlay
+              className='fixed inset-0 bg-neutral-900 opacity-30'
+              aria-hidden='true'
+            />
+            <Dialog.Panel className='relative bg-bgDarkMid rounded-2xl py-6 px-8'>
+              <span className='text-textWhite text-2xl font-bold'>
+                New transaction
+              </span>
 
-            <Dialog.Description className='p-6'>
-              {/* Description */}
-              <NewTxDetails />
-              {/* Description */}
-            </Dialog.Description>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
+              <Dialog.Description className='p-6'>
+                {/* Description */}
+                <NewTxDetails
+                  isOpen={isOpen}
+                  onCloseNewTxModal={onCloseNewTxModal}
+                  isOpenReceiveTxModal={isOpenReceiveTxModal}
+                  handleReceiveTxModal={handleReceiveTxModal}
+                />
+                <ReceiveTxModal
+                  isOpen={isOpenReceiveTxModal}
+                  onClose={handleReceiveTxModal}
+                />
+                {/* Description */}
+              </Dialog.Description>
+            </Dialog.Panel>
+          </div>
+        </Dialog>
+      )}
     </>
   )
 }
