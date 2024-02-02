60d2c0e969413a34c7e7a3c4b664b95826320862
sumi0820 <48553998+sumi0820@users.noreply.github.com>
2024-02-02 12:19:52 +0900

    Fixed error on build app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tailwindcss/tailwind.css';
+import { Buffer } from 'buffer/';
 import App from './App';
 import './lang/configs';
 
